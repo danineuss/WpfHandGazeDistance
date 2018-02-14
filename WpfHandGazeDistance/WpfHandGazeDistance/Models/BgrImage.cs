b93a8df2d467b50f5c4c4f10f3662b32e8954644
Singer  Daniel <dsinger@ethz.ch>
2018-02-14 14:34:52 +0100

    Initial commit

---
+++ WpfHandGazeDistance/WpfHandGazeDistance/Models/BgrImage.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows.Media.Imaging;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace WpfHandGazeDistance.Models
+{
+    /// <summary>
+    /// A basic image container class with BGR images.
+    /// </summary>
+    public class BgrImage
+    {
+        #region Public Properties
+
+        public Image<Bgr, Byte> Image { get; set; }
+
+        #endregion
+
+        #region Public Members
+
+        /// <summary>
+        /// Loads an image from a file path.
+        /// </summary>
+        /// <param name="filePath">The entire file path.</param>
+        public void LoadImage(string filePath)
+        {
+            Image = new Image<Bgr, byte>(filePath);
+        }
+
+        /// <summary>
+        /// Converts the image into a WPF-usable BitmapSource.
+        /// </summary>
+        /// <returns></returns>
+        public BitmapSource ConvertImage()
+        {
+            BitmapSource bitmapSource = BitMapConverter.ToBitmapSource(Image);
+            return bitmapSource;
+        }
+
+        #endregion
+    }
+}
---
+++ WpfHandGazeDistance/WpfHandGazeDistance/ViewModels/HgdViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfHandGazeDistance.ViewModels.Base;
+
+namespace WpfHandGazeDistance.ViewModels
+{
+    public class HgdViewModel : BaseViewModel
+    {
+
+    }
+}
